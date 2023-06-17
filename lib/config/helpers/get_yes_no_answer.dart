2d992593d62ef8a6b28ef510deb0f945e1ab2000
rubbeen <ribenp7@gmail.com>
2023-06-17 13:32:43 -0500

    final

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,14 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
 
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
-    return Message(
-        text: response.data['answer'],
-        fromWho: FromWho.hers,
-        imageUrl: response.data['image']);
+
+
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+    return yesNoModel.toMessageEntity();
   }
 }
--- lib/presentation/screens/chat/her_message_bubble.dart
+++ lib/presentation/screens/chat/her_message_bubble.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../../domain/entities/message.dart';
+
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble({super.key});
+  final Message message;
+
+  const HerMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -13,20 +17,18 @@ class HerMessageBubble extends StatelessWidget {
         Container(
           decoration: BoxDecoration(
               color: colors.secondary, borderRadius: BorderRadius.circular(20)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'Hola Mundo',
-              style: TextStyle(color: Colors.white),
+              message.text,
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
         const SizedBox(
           height: 5,
         ),
-
-        _ImageBubble(),
-
+        _ImageBubble(message.imageUrl!),
         const SizedBox(
           height: 10,
         )
@@ -36,6 +38,10 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+  final String imageUrl;
+
+  const _ImageBubble(  this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -43,7 +49,7 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        'https://yesno.wtf/assets/no/18-1ba72d815ec0e2bff8dba8699a50e275.gif',
+        imageUrl,
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
